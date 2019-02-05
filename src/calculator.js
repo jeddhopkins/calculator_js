e184bbbc2db61ad9d2b49359e938ef4c1c669a4b
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-05 20:28:02 +0000

    test for add method complete

--- src/calculator.js
+++ src/calculator.js
@@ -1,15 +1,2 @@
-function FizzBuzz() {
-};
-
-FizzBuzz.prototype.numbers = function(number) {
-  if (number % 15 === 0) {return 'FizzBuzz';}
-  if (number % 5 === 0) {return 'Buzz';}
-  if (number % 3 === 0) {return 'Fizz';}
-  return number;
-};
-
-let fizzBuzz = new FizzBuzz();
-
-for (let i = 1; i <= 100; i++) {
-  console.log(fizzBuzz.numbers(i));
+function Calculator() {
 };

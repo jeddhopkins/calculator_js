5b82b180456f726683111f50e5c6238e05ce33ae
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-06 09:55:41 +0000

    now throws error when dividing a number by zero

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -9,6 +9,18 @@ var calc = new Calculator();
     it('3 + 3 should equal 6', function() {
       expect(calc.add(3, 3)).toEqual(6);
     });
+    it('(-1) + 5 should equal 4', function() {
+      expect(calc.add(-1, 5)).toEqual(4);
+    });
+    it('5 + (-4) should equal 1', function() {
+      expect(calc.add(5, -4)).toEqual(1);
+    });
+    it('3.6 + 3.6 should equal 7.2', function() {
+      expect(calc.add(3.6, 3.6)).toEqual(7.2);
+    });
+    it('3.63 + 3.67 should equal 7.3', function() {
+      expect(calc.add(3.63, 3.67)).toEqual(7.30);
+    });
   });
 
   describe('subtracting numbers', function() {
@@ -27,6 +39,11 @@ var calc = new Calculator();
     it('10 / 2 should equal 5', function() {
       expect(calc.divide(10, 2)).toEqual(5);
     });
+    it('should throw an error if the number is being divided by zero', function() {
+      expect(function() {
+        calc.divide(10, 0);
+    }).toThrowError("Cannot divide a number by zero");
+  });
   });
 
   describe('square numbers', function() {
--- src/calculator.js
+++ src/calculator.js
@@ -14,7 +14,11 @@ Calculator.prototype.multiply = function (num1, num2) {
 };
 
 Calculator.prototype.divide = function (num1, num2) {
-  return num1 / num2
+  if (num2 === 0) {
+    throw new Error("Cannot divide a number by zero");
+} else {
+    return num1 / num2;
+  };
 };
 
 Calculator.prototype.square = function (num) {
@@ -30,4 +34,4 @@ Calculator.prototype.addThreeNumbers = function (num1, num2, num3) {
 };
 
 let calc = new Calculator
-console.log(calc.sqroot(10));
+console.log(calc.divide(10, 2));
